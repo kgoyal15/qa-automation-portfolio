ea349ac8162741214eed89eca266b59b76f90731
kgoyal15 <kgoyal15@yahoo.com>
2026-03-12 15:14:57 -0400

    Added selenium grid parallel testing

--- selenium-framework/src/main/java/driver/DriverFactory.java
+++ selenium-framework/src/main/java/driver/DriverFactory.java
@@ -1,3 +1,21 @@
-public class DriverFactory {
-    
+public static WebDriver createRemoteDriver(String browser)
+throws MalformedURLException {
+
+    URL gridUrl = new URL("http://localhost:4444/wd/hub");
+
+    if(browser.equalsIgnoreCase("chrome")){
+
+        ChromeOptions options = new ChromeOptions();
+        return new RemoteWebDriver(gridUrl, options);
+
+    }
+
+    if(browser.equalsIgnoreCase("firefox")){
+
+        FirefoxOptions options = new FirefoxOptions();
+        return new RemoteWebDriver(gridUrl, options);
+
+    }
+
+    return null;
 }
